b2b724216ca7c876cc9a7d01207a041d5a383aa3
Fabricio Valerio <fvr.valerio@gmail.com>
2019-08-04 18:58:35 -0400

    reactjs concluido

---
+++ src/pages/Repository/styles.js
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+export const Loading = styled.div`
+  color: #fff;
+  font-size: 30px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+export const Owner = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  a {
+    color: #fb612c;
+    font-size: 16px;
+    font-weight: bold;
+    text-decoration: none;
+  }
+
+  img {
+    width: 120px;
+    border-radius: 50px;
+    margin-top: 20px;
+  }
+
+  h1 {
+    font-size: 24px;
+    margin-top: 10px;
+  }
+
+  p {
+    margin-top: 5px;
+    font-size: 14px;
+    color: #666;
+    line-height: 1.4;
+    text-align: center;
+    max-width: 400px;
+  }
+`;
+export const IssueList = styled.header`
+  padding-top: 30px;
+  margin-top: 30px;
+  border-top: 1px solid #eee;
+  list-style: none;
+
+  li {
+    display: flex;
+    padding: 19px 10px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+
+    & + li {
+      margin-top: 10px;
+    }
+
+    img {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      border: 2px solid #eee;
+    }
+
+    div {
+      flex: 1;
+      margin-left: 15px;
+
+      strong {
+        font-size: 15px;
+
+        a {
+          text-decoration: none;
+          color: #333;
+
+          &:hover {
+            color: #fb612c;
+          }
+        }
+        span {
+          background: #ffc4ae;
+          color: #333;
+          border-radius: 2px;
+          font-size: 12px;
+          font-weight: 600;
+          height: 20px;
+          padding: 3px 4px;
+          margin-left: 10px;
+        }
+      }
+
+      p {
+        margin-top: 5px;
+        font-size: 15px;
+        color: #999;
+      }
+    }
+  }
+`;
